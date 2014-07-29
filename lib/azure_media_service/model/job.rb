6ea4133f4a12962146248c8b4314f5c3e2f50b28
murayama <murayama@logiclogic.jp>
2014-07-29 14:49:17 +0900

    add OutputMediaAssets and InputMediaAssets to job model

--- lib/azure_media_service/model/job.rb
+++ lib/azure_media_service/model/job.rb
@@ -2,6 +2,29 @@ module AzureMediaService
   module Model
     class Job < Base
 
+      def output_assets
+        @output_assets ||= []
+        if @output_assets.empty?
+          url = self.OutputMediaAssets["__deferred"]["uri"].gsub(Config::MEDIA_URI, '')
+          res = @request.get(url)
+          res["d"]["results"].each do |v|
+            @output_assets << Model::Asset.new(v)
+          end
+        end
+        @output_assets
+      end
+
+      def input_assets
+        @input_assets ||= []
+        if @input_assets.empty?
+          url = self.InputMediaAssets["__deferred"]["uri"].gsub(Config::MEDIA_URI, '')
+          res = @request.get(url)
+          res["d"]["results"].each do |v|
+            @input_assets << Model::Asset.new(v)
+          end
+        end
+        @input_assets
+      end
     end
   end
 end

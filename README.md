443530ad9a794f58c22a1a2828e3db889311da0d
murayama <murayama@logiclogic.jp>
2015-05-11 19:48:48 +0900

    add DEPRCATION WARNING

--- README.md
+++ README.md
@@ -22,29 +22,30 @@ Or install it yourself as:
 
 # initialize
 AzureMediaService.configure do |config|
- cofig.id = 'xxxxxxxx' 
+ cofig.id = 'xxxxxxxx'
  config.secret = 'xxxxxxxxxxxxxxxxxx'
 end
 
-# service instance
-ams = AzureMediaService.service
-
 # upload file
+ams = AzureMediaService.service
 asset = ams.upload_media('path/to/example.mp4')
 
+# or
+
+asset = AzureMediaService::Asset.create(asset_name)
+asset.upload('path/to/example.mp4')
+
 # encode job
-asset = ams.assets('nb:cid:UUID:xxxxxxxxxxx-xxxxxxxxxxx-xxxxxx-xxxxxxx')
+asset = AzureMediaService::Asset.get('nb:cid:UUID:xxxxxxxxxxx-xxxxxxxxxxx-xxxxxx-xxxxxxx')
 job = asset.encode_job('H264 Smooth Streaming 720p')
 
 # or
-job = ams.create_encode_job('nb:cid:UUID:xxxxxxxxxxx-xxxxxxxxxxx-xxxxxx-xxxxxxx', 'H264 Smooth Streaming 720p')
+job = AzureMediaService::Job.create('nb:cid:UUID:xxxxxxxxxxx-xxxxxxxxxxx-xxxxxx-xxxxxxx', 'H264 Smooth Streaming 720p')
 
 # publish asset
-asset = ams.assets('nb:cid:UUID:xxxxxxxxxxx-xxxxxxxxxxx-xxxxxx-xxxxxxx')
+asset = AzureMediaService::Asset.get('nb:cid:UUID:xxxxxxxxxxx-xxxxxxxxxxx-xxxxxx-xxxxxxx')
 asset.publish(expire_minutes: 43200)
 
-# or
-ams.publish('nb:cid:UUID:xxxxxxxxxxx-xxxxxxxxxxx-xxxxxx-xxxxxxx', 43200)
 ```
 
 ### Custom Encode Task
@@ -54,9 +55,58 @@ AzureMediaService.configure do |config|
   config.add_encode_task('Custom Encode Task', File.read('path/to/custome_task.xml'))
 end
 
-ams = AzureMediaService.service
-asset = ams.assets('nb:cid:UUID:xxxxxxxxxxx-xxxxxxxxxxx-xxxxxx-xxxxxxx')
+asset = AzureMediaService::Asset.get('nb:cid:UUID:xxxxxxxxxxx-xxxxxxxxxxx-xxxxxx-xxxxxxx')
 asset.encode_job('Custom Encode Task')
+```
+
+### AES Protection
+
+```ruby
+asset = AzureMediaService::Asset.get(asset_id)
+
+key = ScureRandom::random_bytes(16)
+
+content_key = AzureMediaService::ContentKey.create({
+  content_key: key,
+  content_key_type: AzureMediaService::ContentKey::ContentKeyTypes[:EnvelopeEncryption],
+  name: 'content-key-name'
+})
+asset.content_key_link(content_key)
+
+
+content_key_policies = AzureMediaService::ContentKeyAuthorizationPolicy.get()
+content_key_policy = content_key_policies.fin { |p| p.name == 'your policy name' }
+unless content_key_policy
+  content_key_policy = AzureMediaService::ContentKeyAuthorizationPolicy.create('your policy name')
+  policy_options = AzureMediaService::ContentKeyAuthorizationPolicyOption.create({
+    name: 'policy option',
+    key_delivery_type: AzureMediaService::ContentKeyAuthorizationPolicyOption::KeyDeliveryTypes[:BaselineHttp],
+    key_delivery_configuration: nil,
+    restrictions: [
+      {
+        "Name" => "HLS Authorization Policy",
+        "KeyRestrictionType" => AzureMediaService::ContentKeyAuthorizationPolicyOption::KeyRestrictionTypes[:Open]
+      }
+    ]
+  })
+  content_key_policy.option_link(policy_options)
+end
+
+content_key.add_authorization_policy(content_key_policy.Id)
+
+protocols = AzureMediaService::AssetDeliveryPolicy::Protocol
+# Your need to register the key to your key delivery server
+kid = content_key.Id.split(':').last
+key_delivery_url = "your key delivery server url"
+delivery_policy = AzureMediaService::AssetDeliveryPolicy.create({
+  name: 'your delivery policy name',
+  protocol: protocols[:HLS] | protocols[:SmoothStreaming] | protocols[:Dash],
+  policy_type: AzureMediaService::AssetDeliveryPolicy::PolicyType[:DynamicEnvelopeEncryption],
+  configuration: JSON.generate([{Key: AzureMediaService::AssetDeliveryPolicy::ConfigurationKey[:EnvelopeBaseKeyAcquisitionUrl], Vlaue: key_delivery_url}])
+})
+asset.delivery_policy_link(delivery_policy)
+
+asset.publish(expire_minutes: 43200)
 ```
 
 ## Contributing
--- lib/azure_media_service/service.rb
+++ lib/azure_media_service/service.rb
@@ -6,41 +6,45 @@ module AzureMediaService
     end
 
     # assets
-    # def assets(asset_id=nil)
-    #   get('Assets', Model::Asset, asset_id)
-    # end
+    def assets(asset_id=nil)
+      warn("DEPRECATION WARNING: Service#assets is deprecated. Use AzureMediaService::Asset.get() instead.")
+      get('Assets', Asset, asset_id)
+    end
 
     # assets create
-    # def create_asset(name)
-    #   post_body = {
-    #     "Name" => name
-    #   }
-    #   res = @request.post("Assets", post_body)
-    #   Model::Asset.new(res["d"])
-    # end
+    def create_asset(name)
+      warn("DEPRECATION WARNING: Service#create_asset is deprecated. Use AzureMediaService::Asset.create() instead.")
+      post_body = {
+        "Name" => name
+      }
+      res = @request.post("Assets", post_body)
+      Asset.new(res["d"])
+    end
 
     # access policy create
-    # def create_access_policy(name:'Policy', duration_minutes:300, permission:2)
-    #   post_body = {
-    #     "Name" => name,
-    #     "DurationInMinutes" => duration_minutes,
-    #     "Permissions" => permission
-    #   }
-    #   res = @request.post("AccessPolicies", post_body)
-    #   Model::AccessPolicy.new(res["d"])
-    # end
+    def create_access_policy(name:'Policy', duration_minutes:300, permission:2)
+      warn("DEPRECATION WARNING: Service#create_access_policy is deprecated. Use AzureMediaService::AccessPolicy.create() instead.")
+      post_body = {
+        "Name" => name,
+        "DurationInMinutes" => duration_minutes,
+        "Permissions" => permission
+      }
+      res = @request.post("AccessPolicies", post_body)
+      AccessPolicy.new(res["d"])
+    end
 
     # locator create
-    # def create_locator(policy_id:,asset_id:,type:1)
-    #   post_body = {
-    #     "AccessPolicyId" => policy_id,
-    #     "AssetId" => asset_id,
-    #     "Type" => type,
-    #     "StartTime" => (Time.now - 5*60).gmtime.strftime('%Y-%m-%dT%H:%M:%SZ')
-    #   }
-    #   res = @request.post("Locators", post_body)
-    #   Model::Locator.new(res["d"])
-    # end
+    def create_locator(policy_id:,asset_id:,type:1)
+      warn("DEPRECATION WARNING: Service#create_locator is deprecated. Use AzureMediaService::Locator.create() instead.")
+      post_body = {
+        "AccessPolicyId" => policy_id,
+        "AssetId" => asset_id,
+        "Type" => type,
+        "StartTime" => (Time.now - 5*60).gmtime.strftime('%Y-%m-%dT%H:%M:%SZ')
+      }
+      res = @request.post("Locators", post_body)
+      Locator.new(res["d"])
+    end
 
     def upload_media(filepath)
       basename = File.basename(filepath, '.*')
@@ -49,26 +53,30 @@ module AzureMediaService
       asset.upload(filepath)
     end
 
-    # def create_encode_job(asset_id, encode_configuration)
-    #   asset = assets(asset_id)
-    #   asset.encode_job(encode_configuration)
-    # end
-    #
-    # def jobs(job_id=nil)
-    #   get('Jobs', Model::Job, job_id)
-    # end
+    def create_encode_job(asset_id, encode_configuration)
+      warn("DEPRECATION WARNING: Service#create_encode_job is deprecated. Use AzureMediaService::Job.create() instead.")
+      asset = assets(asset_id)
+      asset.encode_job(encode_configuration)
+    end
+
+    def jobs(job_id=nil)
+      warn("DEPRECATION WARNING: Service#jobs is deprecated. Use AzureMediaService::Job.get() instead.")
+      get('Jobs', Job, job_id)
+    end
 
 
     # publish asset
-    # def publish(asset_id, expire_minutes=nil)
-    #   asset = Model::Asset.get(asset_id)
-    #   asset.publish(expire_minutes)
-    # end
-    #
-    # def publish_url(asset_id)
-    #   asset = Model::Asset.get(asset_id)
-    #   asset.publish_url
-    # end
+    def publish(asset_id, expire_minutes=nil)
+      warn("DEPRECATION WARNING: Service#publish is deprecated. Use AzureMediaService::Asset#publish() instead.")
+      asset = Asset.get(asset_id)
+      asset.publish(expire_minutes)
+    end
+
+    def publish_url(asset_id)
+      warn("DEPRECATION WARNING: Service#publish_url is deprecated. Use AzureMediaService::Asset#publish_url() instead.")
+      asset = Asset.get(asset_id)
+      asset.publish_url
+    end
 
     def media_processor_id_by_name(name)
       res = @request.get('MediaProcessors')

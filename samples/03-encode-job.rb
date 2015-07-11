473ec5ee32d0b37a347d0f57ded5a12457187f0e
Khalid Shaikh <kshaikh@rubyinc.org>
2015-07-11 10:36:16 -0500

    Uploads asset and encodes the asset by creating a job.

---
+++ samples/03-encode-job.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+require 'azure_media_service'
+
+p :upload_content
+
+# initialize
+AzureMediaService.configure do |config|
+  # Media Service Account Name
+  config.id = 'thisismytest'
+  # Media Service Access Key
+  config.secret = 'fW5f/kjOY4v5eIVCUZebInIJvbmS49M1+1rlzPLOJsg='
+end
+
+# Test files located at
+# => http://download.wavetlan.com/SVV/Media/HTTP/http-mp4.htm
+
+# Download test file in sample directory
+# wget http://download.wavetlan.com/SVV/Media/HTTP/H264/Talkinghead_Media/H264_test3_Talkingheadclipped_mp4_480x360.mp4
+
+# If missing wget, brew install wget.
+# To install brew: http://brew.sh/
+
+media_file_name = 'H264_test3_Talkingheadclipped_mp4_480x360.mp4'
+
+# Upload file
+asset = AzureMediaService::Asset.create('asset_name')
+asset.upload(media_file_name)
+
+p asset
+
+p :upload_successful
+p :encode_asset
+
+# Encode asset in 720 H264
+p asset.encode_job('H264 Smooth Streaming 720p')
+
+# Verify upload: https://manage.windowsazure.com/
+p :done
